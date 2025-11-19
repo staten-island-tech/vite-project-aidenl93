c82be7faa079a0f49c316484a3122ddbeb0d1047
aidenl93 <aidenl93@nycstudents.net>
2025-11-19 14:01:48 -0500

    we

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,12 +1,20 @@
 import "./style.css";
 const songs = {};
-setupCounter(document.querySelector("#counter"));
+
 function inject(item) {
-  const container = (document.querySelector("#app").innerHTML = ` <h2>song:</h2>
-    <h2>artist: </h2>
+  const container = (document.querySelector("#app").innerHTML = `
+    <h2>song: ${item.title}</h2>
+    <h2>artist: ${item.artist}</h2>
+    
     `);
 }
-
+const body = document.querySelector(".body");
+const darkmode = document.querySelector(".darkmode");
+darkmode.addEventListener("click", () => {
+  body.style.backgroundColor = "black";
+  darkmode.style.visibility = "hidden";
+  document.insertAdjacentHTML("afterbegin", `<button>hey</button>`);
+});
 /* 
 make Array
 inject songs for each item in the Array */
